8b55502e3f7c75a4d5bfc6d53e7a699547870e35
Chandrakanth540 <104191491+Chandrakanth540@users.noreply.github.com>
2024-05-12 17:28:03 +0530

    Update controller.js

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,27 +1,27 @@
 let collection = require('../model/user');
 
-const postUser = (req, res) => { // Changed from postCat
-    let user = req.body; // Changed from cat
-    collection.postUser(user, (err, result) => { // Changed from postCat
+const postUser = (req, res) => { 
+    let user = req.body; 
+    collection.postUser(user, (err, result) => { 
         if (!err) {
-            res.json({ statusCode: 200, data: result, message: 'post user success' }); // Changed from 'post cat success'
+            res.json({ statusCode: 200, data: result, message: 'post user success' }); 
         }
     });
 };
 
-const getAllUsers = (req, res) => { // Changed from getAllCats
-    collection.getAllUsers((err, result) => { // Changed from getAllCats
+const getAllUsers = (req, res) => { 
+    collection.getAllUsers((err, result) => { 
         if (!err) {
-            res.json({ statusCode: 200, data: result, message: 'get user success' }); // Changed from 'get cat success'
+            res.json({ statusCode: 200, data: result, message: 'get user success' });
         }
     });
 };
 
-const deleteUser = (req, res) => { // Changed from deleteCat
-    let user = req.body; // Changed from cat
-    collection.deleteUser(user, (err, result) => { // Changed from deleteCat
+const deleteUser = (req, res) => { 
+    let user = req.body; 
+    collection.deleteUser(user, (err, result) => { 
         if (!err) {
-            res.json({ statusCode: 200, data: result, message: 'delete user success' }); // Changed from 'delete cat success'
+            res.json({ statusCode: 200, data: result, message: 'delete user success' }); 
         }
     });
 };
